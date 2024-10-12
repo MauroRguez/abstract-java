e168039fd03460d061bb2c7bea37a1ce37b7bcef
santiago sierra cano <santiscano@gmail.com>
2024-10-11 20:49:02 -0500

    agregue la clase con mi nombre

---
+++ src/Santiago.java
@@ -0,0 +1,10 @@
+public abstract class Santiago {
+
+    // Abstract method (does not have a body)
+    public abstract void performAction();
+
+    // Regular method
+    public void displayMessage() {
+        System.out.println("Hello from Santiago!");
+    }
+}
